61e359b4353038fde30974e1fa8cabddaf41eea0
Sultepronit <sultepronit@gmail.com>
2023-03-18 13:35:43 +0200

    now we can add new comment by pressing enter key

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,19 +1,29 @@
+function addNewComment() {
+	//console.log("Hello World!");
+	
+	var comment_text = $(".comment-input input").val();
+	var $new_comment;
+	
+	if(comment_text !== "") {
+		$new_comment = $("<p>").text(comment_text);
+		$(".comments").append($new_comment);
+		$(".comment-input input").val("");
+	}
+}
+
 var main = function () {
 	"use strict";
 	
 	$(".comment-input button").on("click", function (event) {
-		//console.log("Hello World!");
-		
-		var comment_text = $(".comment-input input").val();
-		var $new_comment;
-		
-		if(comment_text !== "") {
-			$new_comment = $("<p>").text(comment_text);
-			$(".comments").append($new_comment);
-			$(".comment-input input").val("");
+		addNewComment();
+	});
+	
+	$(".comment-input input").on("keypress", function (event) {
+		//console.log("keyCode: " + event.keyCode);
+		if(event.keyCode === 13) {
+			//console.log("Enter!");
+			addNewComment();
 		}
-		
-		
 	});
 };
 
