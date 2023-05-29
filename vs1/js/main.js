6a623484f0f46ec3daa5a7220cc77f232721ad9c
Sultepronit <sultepronit@gmail.com>
2023-05-29 19:56:47 +0300

    learned/trained some basic string methods

--- vs1/js/main.js
+++ vs1/js/main.js
@@ -1,4 +1,16 @@
 console.log('Hello, dudes!');
 console.log(typeof "main");
 console.log(typeof 45);
-console.log(typeof true);
+console.log(typeof true); 
+console.log("something".charAt(3));
+console.log('something'.charCodeAt(3)); // 101
+console.log('something'.indexOf('thi')); // 4
+console.log('google'.lastIndexOf('g')); // 3
+console.log('google'.slice(3)); // gle
+console.log('google'.slice(1,4)) //oog
+console.log('England'.toUpperCase());
+console.log('England'.toLowerCase());
+console.log('google'.includes('oo')); // true
+console.log('google'.includes('gee')); // false
+console.log('google'.split('o')); // ['g', '', 'le']
+console.log('GooGle'.split('')); // ['G', 'o', 'o', 'G', 'l', 'e']
