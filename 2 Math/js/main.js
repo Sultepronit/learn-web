30276d13ac95877584a79d32513ee3b42706a62c
Sultepronit <sultepronit@gmail.com>
2023-06-02 08:20:37 +0300

    lernead some math

---
+++ 2 Math/js/main.js
@@ -0,0 +1,11 @@
+console.log(Math.PI);
+console.log(Math.trunc(5.98)); // 5
+console.log(Math.round(4.5)); // 5
+console.log(Math.ceil(3.1)); // 4
+console.log(Math.floor(6.99999)); // 6
+console.log(Math.trunc(-5.3)); // -5
+console.log(Math.ceil(-5.3)); // -5
+console.log(Math.floor(-5.3)); // -6
+console.log(Math.pow(2, 8)); // 256 // power
+console.log(Math.min(0.5, 7, 99, -1)); // -1
+console.log(Math.max(5, 7.7, 7, -5)); // 7.7
