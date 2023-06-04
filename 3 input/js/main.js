2bcb6e4a921c31a8792354ccf48e573ba45e1590
Sultepronit <sultepronit@gmail.com>
2023-06-04 09:03:55 +0300

    lerned about js input

---
+++ 3 input/js/main.js
@@ -0,0 +1,10 @@
+//alert('output!');
+/*let bool = confirm('Ok = True\nCancel = False');
+console.log(bool);*/
+
+let name = prompt('Please, enter your name');
+console.log(name);
+console.log('|' + name + '|');
+console.log('|' + name.trim() + '|');
+console.log(name ?? "You din't enter your name!");
+                // if null
