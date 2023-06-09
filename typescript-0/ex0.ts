2c607378e5eb644ee105076d06853b2fb6004c0d
Sultepronit <sultepronit@gmail.com>
2023-06-09 09:32:04 +0300

    ts hellow world

---
+++ typescript-0/ex0.ts
@@ -0,0 +1,9 @@
+let myName: string = 'aNNa';
+//myName = 47; // not assignable
+let secondName = 'LiLi';
+//secondName = 49;
+let something = JSON.parse('99');
+console.log(typeof something);
+
+const C = 'constant';
+console.log(C);
