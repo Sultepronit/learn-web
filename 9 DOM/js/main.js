a81e85987a27104f84a18a1f4dd80e23cafa7d8c
Sultepronit <sultepronit@gmail.com>
2023-06-12 15:09:39 +0300

    started learning DOM manipulation

---
+++ 9 DOM/js/main.js
@@ -0,0 +1,11 @@
+// Document Object Model
+
+'use strict';
+
+const view1 = document.getElementById('view1');
+console.log(view1);     
+const view2 = document.querySelector('#view2');
+console.log(view2);
+
+view1.style.display = 'none';
+view2.style.display = 'flex';
