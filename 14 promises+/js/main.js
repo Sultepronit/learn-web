4c24fbe75ef1f4772b69f5bd2cd2ffededbc2c46
Sultepronit <sultepronit@gmail.com>
2023-06-21 14:23:52 +0300

    learned about promises, fetch, async, await...

--- 14 promises+/js/main.js
+++ 14 promises+/js/main.js
@@ -10,11 +10,8 @@ const myPromise = new Promise((resolve, reject) => {
         reject('Rejected!');
     }
 });
-
 console.log(myPromise); // Promise {<fulfilled>: 'Resolved!'}
-
 myPromise.then(value => console.log(value)); // Resolved!
-
 myPromise.then(value => (value + '+'))
     .then(value2 => console.log(value2)); // Resolved!+
 
@@ -27,9 +24,7 @@ const myRejectedPromise = new Promise((resolve, reject) => {
         reject('Rejected!');
     }
 });
-
 console.log(myRejectedPromise); // Promise {<fulfilled>: 'Resolved!'}
-
 myRejectedPromise.then(value => (value + '+'))
     .then(value2 => console.log(value2))
     .catch(err => console.error(err)); // Rejected!
@@ -39,7 +34,6 @@ const myWaitingForPromise = new Promise((resolve, reject) => {
         resolve('3 seconds passed!');
     }, 3000);
 });
-
 console.log(myWaitingForPromise); // Promise {<pending>}
 myWaitingForPromise.then(value => console.log(value)); // 3 seconds passed!
 
@@ -53,3 +47,131 @@ users.then(response => {
 }).then(data => console.log(data)); // (10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
 
 console.log(users); // Promise {<pending>}
+
+// Async, Await
+const myFunction = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const userData = await response.json();
+    //console.log(userData); // (10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+    return userData;
+}
+
+async function secondFunction() {
+    const data = await myFunction();
+    console.log(data); // (10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+}
+secondFunction();
+
+// example 1 
+async function getAllUserEmails() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const userData = await response.json();
+    const userEmailArray = userData.map(user => user.email);
+
+    console.log(userEmailArray); // (10) ['Sincere@april.biz', 'Shanna@melissa.tv', ......
+
+    publicData(userEmailArray);
+
+    return(userEmailArray);
+}
+
+function publicData(data) {
+    console.log(data); // (10) ['Sincere@april.biz', 'Shanna@melissa.tv', ......
+}
+
+console.log(getAllUserEmails()); // Promise {<pending>}
+
+// example 2
+async function getDadJoke() {
+    const response = await fetch('https://icanhazdadjoke.com/', {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json'
+        }
+    });
+    const joke = await response.json();
+    console.log(joke);
+    console.log(joke.joke);
+
+    const response2 = await fetch('https://icanhazdadjoke.com/', {
+        method: 'GET',
+        headers: {
+            Accept: 'text/plain'
+        }
+    });
+    const joke2 = await response2.text();
+    console.log(joke2);
+}
+getDadJoke();
+
+// example 3
+async function postData() {
+    const obj = {
+        id: 'scaFQfFlqjb',
+        joke: 'My dentist is the best, he even has a little plaque!',
+        status: 200
+    };
+    const response = await fetch('https://httpbin.org/post', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    });
+    const res = await response.json();
+    console.log(res);
+}
+postData();
+
+// example 4
+//another site now, doesn't accept other names!
+const requestJoke = async (firstName, lastName) => {
+    const response = await fetch(`https://api.chucknorris.io/jokes/random?firstName=${firstName}&lastName=${lastName}`);
+    const joke = await response.json();
+    console.log(joke.value);
+    //console.log(joke.value.joke);
+}
+requestJoke('Bruce', 'Lee');
+
+// special example!
+async function getMyGoogleData() {
+    const myApp = "https://script.google.com/macros/s/AKfycby5Gf4_PVOZxohbTHplidiOrOQBZXOAmTRx4QhJD-nFlz8Lvq6ycu2evra0nXPeA4fB/exec";
+    const action = "getTasks";
+    const url = myApp + "?action=" + action;
+    const parameters = {
+        method: 'GET',
+        headers: {
+                Accept: 'application/x-www-form-urlencoded'
+        }
+    }
+    //const response = await fetch(url, parameters);
+    const response = await fetch(url);
+    //console.log(response);      
+    const array = await response.json();
+    console.log(array);
+}
+//getMyGoogleData();
+
+// special example 2!
+async function sendToGoogleEngCell(num, col, newValue) {
+    const myApp = "https://script.google.com/macros/s/AKfycby5Gf4_PVOZxohbTHplidiOrOQBZXOAmTRx4QhJD-nFlz8Lvq6ycu2evra0nXPeA4fB/exec";
+    const action = "toCelll";
+	const body = 'num=' + encodeURIComponent(num) + '&col=' + encodeURIComponent(col) +
+	'&newValue=' + encodeURIComponent(newValue) + '&action=' + encodeURIComponent(action);
+    const url = myApp + '?' + body;
+    const parameters = {
+        method: 'POST',
+        headers: {
+            Accept: 'application/x-www-form-urlencoded'
+        }
+    }
+    try {
+        const response = await fetch(url, parameters);
+        console.log(response);  
+    } catch(err) {
+        //console.log(err);
+    }
+    
+}
+// working, though with same error as old good method....
+//sendToGoogleEngCell(1, 'Z', '!!!'); 
