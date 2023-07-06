f10474b0e25dcf0d5ef792b4104a32882af21e59
Sultepronit <sultepronit@gmail.com>
2023-07-06 12:23:44 +0300

    trained using event emitter, and saving logs to file

---
+++ node/4 event emitter/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const logEvents = require('./logEvents');
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {};
+
+const myEmitter = new MyEmitter();
+
+// add listener for the log event
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(() => {
+    // emit event
+    myEmitter.emit('log', 'Log event emitted!');
+}, 2000);
