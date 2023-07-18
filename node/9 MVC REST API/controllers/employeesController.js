33b16d75b479fa2ec417aa0c5d20b95e0a8409df
Sultepronit <sultepronit@gmail.com>
2023-07-18 21:15:51 +0300

    first half of lesson on MVC

---
+++ node/9 MVC REST API/controllers/employeesController.js
@@ -0,0 +1,38 @@
+const data = {
+    employees: require('../model/employees.json'),
+    setEmployees: function (data) { this.employees = data }
+}
+
+const getAllEmployees = (req, res) => {
+    res.json(data.employees);
+}
+
+const createNewEmployee = (req, res) => {
+    res.json({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+    });
+}
+
+const updateEmployee = (req, res) => {
+    res.json({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+    })
+}
+
+const deleteEmployee = (req, res) => {
+    res.json({ id: req.body.id });
+}
+
+const getEmployee = (req, res) => {
+    res.json({ id: req.params.id });
+}
+
+module.exports = {
+    getAllEmployees,
+    createNewEmployee,
+    updateEmployee,
+    deleteEmployee,
+    getEmployee
+}
