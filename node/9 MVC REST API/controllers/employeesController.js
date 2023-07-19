f40ee4928cd2f7ad8a306dabd0201ff69e9bccdd
Sultepronit <sultepronit@gmail.com>
2023-07-19 14:01:18 +0300

    learned several things about MVC REST API

--- node/9 MVC REST API/controllers/employeesController.js
+++ node/9 MVC REST API/controllers/employeesController.js
@@ -8,25 +8,66 @@ const getAllEmployees = (req, res) => {
 }
 
 const createNewEmployee = (req, res) => {
-    res.json({
+    const lei = data.employees.length - 1;
+    const newEmployee = {
+        id: lei < 0 ? 1 : data.employees[lei].id + 1,
         firstName: req.body.firstName,
         lastName: req.body.lastName
-    });
+    }
+
+    if(!newEmployee.firstName || !newEmployee.lastName) {
+        return res.status(400).json({message: 'First and last names are required!'});
+    }
+
+    data.setEmployees([...data.employees, newEmployee]);
+
+    // here we can save results
+
+    res.status(201).json(data.employees);
 }
 
 const updateEmployee = (req, res) => {
-    res.json({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName
-    })
+    if(!req.body.id || !(req.body.firstName || req.body.lastName)) {
+        return res.status(400).json({ messgae: 'Wrong input!' });
+    }
+    const nl = [...data.employees];
+    for(let i = 0; i <= nl.length; i++) {
+        if(i === nl.length) {
+            return res.status(400).json({ message: `Employee ID ${req.body.id} not found!` });
+        }
+        if(nl[i].id === parseInt(req.body.id)) {
+            if(req.body.firstName) nl[i].firstName = req.body.firstName;
+            if(req.body.lastName) nl[i].lastName = req.body.lastName;
+            break;
+        }
+    }
+    data.setEmployees(nl);
+    res.status(201).json(data.employees);
 }
 
 const deleteEmployee = (req, res) => {
-    res.json({ id: req.body.id });
+    if(!req.body.id) return res.status(400).json({ message: 'Wrong input!' });
+    const nl = [...data.employees];
+    for(let i = 0; i <= nl.length; i++) {
+        if(i === nl.length) {
+            return res.status(400).json({ message: `Employee ID ${req.body.id} not found!` });
+        }
+        if(nl[i].id === parseInt(req.body.id)) {
+            nl.splice(i, 1);
+            break;
+        }
+    }
+    data.setEmployees(nl);
+    res.json(data.employees);
 }
 
 const getEmployee = (req, res) => {
-    res.json({ id: req.params.id });
+    if(!req.params.id) return res.status(400).json({ message: 'Wrong input!' });
+    const employee = data.employees.find(e => e.id === parseInt(req.params.id));
+    if(!employee) {
+        return res.status(400).json({ message: `Employee ID ${req.params.id} not found!` });
+    }
+    res.json(employee);
 }
 
 module.exports = {
