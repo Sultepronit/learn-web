aee84848ca5c94856c836858c3cac095d0119ea4
Sultepronit <sultepronit@gmail.com>
2023-07-19 08:33:25 +0300

    added highlighting to selected button

--- react-course/4-second-project/src/App.js
+++ react-course/4-second-project/src/App.js
@@ -5,17 +5,26 @@ import List from './List';
 
 function App() {
   const [loadingDb, setLoadingDb] = useState(false);
-  //const [items, setItems] = useState(['one', 'two', 'three']);
   const [items, setItems] = useState([]);
+  const defaultClasses = {
+    'users': '',
+    'posts': '',
+    'comments': ''
+  };
+  const [butClasses, setButClasses] = useState(defaultClasses);
 
   function clickedButton(listName) {
     getData(listName, setLoadingDb, setItems);
+    const n = { ...defaultClasses };
+    n[listName] = 'selected'; 
+    setButClasses(n);
   }
 
   return (
     <div className="App">
       <header>
         <Buttons
+          classes={butClasses}
           clicked={clickedButton}
         />
       </header>
--- react-course/4-second-project/src/Buttons.js
+++ react-course/4-second-project/src/Buttons.js
@@ -1,10 +1,11 @@
-function Buttons({ clicked }) {
+function Buttons({ classes, clicked }) {
     const names = ['users', 'posts', 'comments'];
     return (
         names.map(name =>
             <button
-            key={name}
-            onClick={() => clicked(name)}
+                key={name}
+                className={classes[name]}
+                onClick={() => clicked(name)}
             >
                 {name}
             </button>
