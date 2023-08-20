f9aa4d095cdccceb016b8f86c0888bf34d19987a
Sultepronit <sultepronit@gmail.com>
2023-08-20 15:47:13 +0300

    finished....

--- node/13 mongodb/controllers/employeesController.js
+++ node/13 mongodb/controllers/employeesController.js
@@ -1,7 +1,7 @@
 const Employee = require('../model/Employee');
 
 const getAllEmployees = async (req, res) => {
-    const employees = await Employee.Find();
+    const employees = await Employee.find();
     if(!employees) return res.status(204).json({ message: 'No employees found.'});
     res.json(employees);
 }
@@ -22,48 +22,50 @@ const createNewEmployee = async (req, res) => {
     }
 }
 
-const updateEmployee = (req, res) => {
-    if(!req.body.id || !(req.body.firstName || req.body.lastName)) {
-        return res.status(400).json({ messgae: 'Wrong input!' });
+const updateEmployee = async (req, res) => {
+    if(!req?.body?.id) {
+        return res.status(400).json({ message: 'ID is required!' });
     }
-    const nl = [...data.employees];
-    for(let i = 0; i <= nl.length; i++) {
-        if(i === nl.length) {
-            return res.status(400).json({ message: `Employee ID ${req.body.id} not found!` });
-        }
-        if(nl[i].id === parseInt(req.body.id)) {
-            if(req.body.firstName) nl[i].firstName = req.body.firstName;
-            if(req.body.lastName) nl[i].lastName = req.body.lastName;
-            break;
-        }
-    }
-    data.setEmployees(nl);
-    res.status(201).json(data.employees);
-}
 
-const deleteEmployee = (req, res) => {
-    if(!req.body.id) return res.status(400).json({ message: 'Wrong input!' });
-    const nl = [...data.employees];
-    for(let i = 0; i <= nl.length; i++) {
-        if(i === nl.length) {
-            return res.status(400).json({ message: `Employee ID ${req.body.id} not found!` });
-        }
-        if(nl[i].id === parseInt(req.body.id)) {
-            nl.splice(i, 1);
-            break;
-        }
-    }
-    data.setEmployees(nl);
-    res.json(data.employees);
-}
-
-const getEmployee = (req, res) => {
-    if(!req.params.id) return res.status(400).json({ message: 'Wrong input!' });
-    const employee = data.employees.find(e => e.id === parseInt(req.params.id));
+    const employee = await Employee.findOne({ _id: req.body.id }).exec();
     if(!employee) {
-        return res.status(400).json({ message: `Employee ID ${req.params.id} not found!` });
+        return res.status(204).json({ message: `No employee matches ID ${req.body.id}.` });
     }
-    res.json(employee);
+    if(req.body?.firstName) employee.firstName = req.body.firstName;
+    if(req.body?.lastName) employee.lastName = req.body.lastName;
+    const result = await employee.save();
+    res.json(result);
+}
+
+const deleteEmployee = async (req, res) => {
+    if(!req?.body?.id) return res.status(400).json({ message: 'Epmployee ID required'});
+
+    const employee = await Employee.findOne({ _id: req.body.id }).exec();
+    if(!employee) {
+        return res.status(204).json({ message: `No employee matches ID ${req.body.id}.` });
+    }
+
+    const result = await employee.deleteOne({ _id: req.body.id });
+    res.json(result);
+}
+
+const getEmployee = async (req, res) => {
+    if(!req?.params?.id) return res.status(400).json({ message: 'Employee ID required' });
+    //console.log(req.params.id);
+
+    // need to be done in other places to escape error
+    // in case of incorrect ID
+    try {
+        const employee = await Employee.findOne({ _id: req.params.id }).exec();
+        if(!employee) {
+            console.log(`No employee matches ID ${req.params.id}.`);
+            return res.status(204).json({ message: `No employee matches ID ${req.params.id}.`});
+        }
+        res.json(employee);
+    } catch(err) {
+        console.log(err);
+        res.status(204).json({ message: `Error happend while searching for entry with ID ${req.params.id}.` });
+    }
 }
 
 module.exports = {
