65f86a070e2e64d9681ce11fe853e32e91ac9879
Sultepronit <sultepronit@gmail.com>
2023-08-27 15:33:09 +0300

    created project from previous; added data in json format, handled by json server, and axios API that gets the data

---
+++ react-course/6-axious/src/App.js
@@ -0,0 +1,97 @@
+// npm i react-router-dom -S
+// npm i react-router-dom@5.2.0 !!!!!!
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import About from './About';
+import Missing from './Missing';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+// npm i date-fns -S
+import { format } from 'date-fns';
+import api from './api/posts';
+
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get('/posts');
+        setPosts(response.data);
+      } catch(err) {
+        if(err.response) {
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.responce.headers);
+        } else {
+          console.error(err);
+        }
+      }
+    }
+    fetchPosts();
+  }, []);
+
+  useEffect(() => {
+    const filteredResults = posts.filter(post => 
+      post.body.toLowerCase().includes(search.toLowerCase())
+      || post.title.toLowerCase().includes(search.toLowerCase())
+    );
+
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    const allPosts = [ ...posts, newPost ];
+    setPosts(allPosts);
+    setPostTitle('');
+    setPostBody('');
+    navigate('/');
+  }
+
+  const handleDelete = (id) => {
+    const postList = posts.filter(post => post.id !== id);
+    setPosts(postList);
+    navigate('/');
+  }
+  
+  return (
+    <div className="App">
+      <Header title="React JS Blog" />
+      <Nav search={search} setSearch={setSearch} />
+      <Routes>
+        <Route path="/" element={
+          <Home /* posts={posts} */ posts={searchResults} />
+        } />
+        <Route path="/post" element={<NewPost
+          handleSubmit={handleSubmit}
+          postTitle={postTitle}
+          setPostTitle={setPostTitle}
+          postBody={postBody}
+          setPostBody={setPostBody}
+        />} />
+        <Route path="/post/:id" element={
+          <PostPage posts={posts} handleDelete={handleDelete} />
+        } />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
