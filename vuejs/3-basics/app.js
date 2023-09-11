506abcceb039e4395b095714d9ad1f52cf1d2106
Sultepronit <sultepronit@gmail.com>
2023-09-11 11:12:59 +0300

    added style form vue

--- vuejs/3-basics/app.js
+++ vuejs/3-basics/app.js
@@ -4,12 +4,19 @@ const vm = Vue.createApp({
 	data() {
 		return {
 			isPurple: false,
-			selectedColor: ''
+			selectedColor: '',
+			size: 100
 		}
 	},
 	computed: {
 		block_classes() {
 			return { purple: this.isPurple };
+		},
+		additionalStyle() {
+			return [
+				{ width: this.size + 'px', height: this.size + 'px'},
+				{ transform: 'rotate(30deg)'}
+			];
 		}
 	}
 }).mount('#app');
