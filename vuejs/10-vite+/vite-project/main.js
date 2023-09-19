7e793d0f0835925c8de68a7774b88f8c5e05ef56
Sultepronit <sultepronit@gmail.com>
2023-09-19 21:41:20 +0300

    learned very basic Sass concepts

--- vuejs/10-vite+/vite-project/main.js
+++ vuejs/10-vite+/vite-project/main.js
@@ -1,6 +1,7 @@
-//npm create vite$latest
+// npm create vite$latest
+// npm install sass
 
-import './style.css'
+import './style.scss'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
@@ -13,7 +14,7 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Hello <span>Vite</span>!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
