f3f92eacd409f8e0067dc8ca3ea7959038cb9a04
Sultepronit <sultepronit@gmail.com>
2023-09-21 15:37:32 +0300

    learned more about int

--- php/1.1-basics/data_types.php
+++ php/1.1-basics/data_types.php
@@ -19,3 +19,39 @@ if ($isTrue) {
 }
 
 # false: 0, -1, 0.0, -0.0, '0', '', [], null
+
+#########################################
+# integer/int
+$x = 0x2a;
+echo '<br>', $x; # 42
+$y = 011;
+echo '<br>', $y; # 9
+$z = 0b111;
+echo '<br>', $z; # 7
+
+echo '<br>', PHP_INT_MAX; # 9223372036854775807
+$big = PHP_INT_MAX + 1;
+echo '<br>', $big; # 9.2233720368548E+18
+echo '<br>'; var_dump($big); # float(9.223372036854776E+18)
+
+# casting
+$int = (integer) true;
+echo '<br>'; var_dump($int); # int(1)
+$int2 = (int) 5.99;
+echo '<br>'; var_dump($int2); # int(5)
+$int3 = (int) '6.99';
+echo '<br>'; var_dump($int3); # int(6)
+$int4 = (int) '7,99';
+echo '<br>'; var_dump($int4); # int(7)
+$int5 = (int) '45cd';
+echo '<br>'; var_dump($int5); # int(45)
+$int6 = (int) 'ab45cd';
+echo '<br>'; var_dump($int6); # int(0)
+echo '<br>'; var_dump( (int) null ); # int(0)
+
+echo '<br>', is_int(-1); # 1
+echo '<br>[', is_integer(-1.5), ']'; # []
+
+$int7 = 100_000;
+echo '<br>', $int7; # 100000
+echo '<br>', (int) '100_000'; # 100
