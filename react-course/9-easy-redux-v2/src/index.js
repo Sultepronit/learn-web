a50a31e66b30fccbd461d25a411bf73f9b79a234
Sultepronit <sultepronit@gmail.com>
2023-09-22 10:16:44 +0300

    continue to implement easy-peasy in the project

--- react-course/9-easy-redux-v2/src/index.js
+++ react-course/9-easy-redux-v2/src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { StoreProvider } from 'easy-peasy';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      {/* <Route path="/" conponent={App} /> */}
-      <App />
-    </Router>
+    <StoreProvider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </StoreProvider>
   </React.StrictMode>
 );
 
