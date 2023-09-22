e787e20c90addda00c6953448acd849c78c99f33
Sultepronit <sultepronit@gmail.com>
2023-09-22 18:22:37 +0300

    learned more about float

--- php/1.1-basics/data_types.php
+++ php/1.1-basics/data_types.php
@@ -55,3 +55,56 @@ echo '<br>[', is_integer(-1.5), ']'; # []
 $int7 = 100_000;
 echo '<br>', $int7; # 100000
 echo '<br>', (int) '100_000'; # 100
+
+#################################
+echo '<br>----------------float/double----------------';
+$x = 5.6;
+echo '<br>', $x;
+$y = 5.6e3;
+echo '<br>', $y; # 5600
+echo '<br>'; var_dump($y); # float(5600)
+$z = 5.6e-3;
+echo '<br>', $z; # 0.0056
+$f1 = 1_000.5e2;
+echo '<br>', $f1; # 100050
+
+echo '<br>', PHP_FLOAT_MAX; # 1.7976931348623E+308
+echo '<br>', PHP_FLOAT_MIN; # 2.2250738585072E-308
+
+echo '<br>', (0.1 + 0.7); # 0.8
+echo '<br>', var_dump(0.1 + 0.7); # float(0.7999999999999999)
+echo '<br>', floor((0.1 + 0.7) * 10); # 7
+echo '<br>', (0.1 + 0.2); # 0.3
+echo '<br>', var_dump(0.1 + 0.2); # float(0.30000000000000004)
+echo '<br>', ceil((0.1 + 0.2) * 10); # 4
+
+$f2 = 0.23;
+$f3 = 1 - 0.77;
+echo '<br>'; var_dump($f2, $f3); # float(0.23) float(0.22999999999999998)
+echo '<br>';
+if($f2 == $f3) echo 'Equal!'; else echo 'Not equal!'; # Not equal!
+
+echo '<br>', log(-1); # NAN
+// echo '<br>', 1 / 0; # Fatal error
+$f4 = PHP_FLOAT_MAX * 2;
+echo '<br>', $f4; # INF
+echo '<br>'; var_dump($f4); # float(INF)
+echo '<br>', is_infinite($f4); # 1
+echo '<br>', is_finite($f3); # 1
+echo '<br>', is_nan(log(-1)); # 1
+
+# casting
+$int1 = 5;
+echo '<br>'; var_dump($int1); # int(5)
+$f5 = (float) $int1;
+echo '<br>'; var_dump($f5); # float(5)
+echo '<br>'; var_dump(floatval($int1)); # int(5)
+
+echo '<br>'; var_dump((float) '75.4abd'); # float(75.4)
+echo '<br>'; var_dump((float) '75,4abd'); # float(75)
+echo '<br>'; var_dump((float) 't75,4abd'); # float(0)
+
+
+
+
+
