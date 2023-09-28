0fd81322a38734dd0b2099728e522527c15e8842
Sultepronit <sultepronit@gmail.com>
2023-09-28 08:34:19 +0300

    learned if else inside html

--- php/1.1-basics/html.php
+++ php/1.1-basics/html.php
@@ -9,5 +9,15 @@
     <?php echo 'Long way' ?>
     <br>
     <?= 'Short way' ?>
+
+    <?php $number = 10 ?>
+    <?php if ($number >= 1000): ?>
+        <p>1000+</p>
+    <?php elseif($number >= 100): ?>
+        <p>100+</p>
+    <?php else: ?>
+        <p>less than 100!</p>
+    <?php endif ?>
+
 </body>
 </html>
