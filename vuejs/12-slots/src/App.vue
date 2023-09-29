2a63bc429afe9eae2099ed6dca5002fdda3f55f1
Sultepronit <sultepronit@gmail.com>
2023-09-29 13:12:38 +0300

    learned concept of vue slots

---
+++ vuejs/12-slots/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <app-form>
+    <div class="help">
+      <p>Some help text.</p>
+    </div>
+    <div class="fields">
+      <input type="text" placeholder="email">
+      <input type="text" placeholder="username">
+      <input type="password" placeholder="password">
+    </div>
+    <div class="buttons">
+      <button type="submit">Submit</button>
+    </div>
+  </app-form>
+
+  <app-form>
+    
+  </app-form>
+</template>
+
+<script>
+import AppForm from './components/Form.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppForm
+  }
+}
+</script>
