0cd034c927ee37f6a025e3bbfdf6505c5efba2f4
Sultepronit <sultepronit@gmail.com>
2023-09-30 20:42:54 +0300

    learned dynamic components in vue

---
+++ vuejs/13-dynamic-components/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <select v-model="componentName">
+    <option value="HomeComponent">Home</option>
+    <option value="AboutComponent">About</option>
+  </select>
+  
+  <keep-alive>
+    <component :is="componentName" />
+  </keep-alive>
+
+</template>
+
+<script>
+import HomeComponent from './components/HomeComponent.vue';
+import AboutComponent from './components/AboutComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomeComponent,
+    AboutComponent
+  },
+  data() {
+    return {
+      componentName: 'HomeComponent'
+    }
+  }
+}
+</script>
---
+++ vuejs/13-dynamic-components/src/components/HomeComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Home Page</h1>
+</template>
+
+<script>
+export default {
+    unmounted() {
+        // with keep-alive wouldn't appear
+        console.log('Home component unmounted');
+    },
+    activated() {
+        console.log('Home component activated');
+    },
+    deactivated() {
+        console.log('Home component deactivated');
+    }
+}
+</script>
