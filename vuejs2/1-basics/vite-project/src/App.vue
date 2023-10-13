e97fa796f02953ac3c25c32d2ee6f5f5e895aa8c
Sultepronit <sultepronit@gmail.com>
2023-10-13 12:48:46 +0300

    learned basics of deirctives and reactivity usage in vue, composition API

--- vuejs2/1-basics/vite-project/src/App.vue
+++ vuejs2/1-basics/vite-project/src/App.vue
@@ -4,8 +4,12 @@ import { ref } from 'vue';
 const text1 = ref('some text!');
 const text2 = ref('This text is <u><i>formatted</u></i>');
 const p1class = ref('red');
-//const p2class = ref('blue');
-const p2class = 'blue'; //at this point don't know what the ref() does
+const p2class = 'blue'; // at this point don't know what the ref() does
+
+console.log(p1class); // RefImpl {__v_isShallow: false, dep: undefined, __v_isRef: true, _rawValue: 'red', _value: 'red'}
+console.log(p1class.value); // red
+console.log(p2class); // blue
+
 const p3classId = {
   id: 'third-p',
   class: 'green'
@@ -18,6 +22,19 @@ const clickPlaceholder = 'click';
 function clicked() {
   console.log('Here we go!');
 }
+
+const buttonColor = ref('green');
+function changeColor() {
+  console.log('change!');
+  if(buttonColor.value === 'green') {
+    buttonColor.value = 'red';
+  } else {
+    buttonColor.value = 'green';
+  }
+}
+
+const count0 = 0;
+const count1 = ref(0);
 
 </script>
 
@@ -35,10 +52,23 @@ function clicked() {
   <button v-on:[clickPlaceholder]="clicked()">
     Click me!
   </button>
+
+  <button @click="changeColor()" :class="buttonColor">
+    Change me!
+  </button>
+
+  <p>{{ count0 }}</p>
+  <button @click="count0++">increment?</button>
+
+  <p>{{ count1 }}</p>
+  <button @click="count1++">increment!</button>
   
 </template>
 
 <style scoped>
+  button {
+    font-size: 1.5rem;
+  }
   .red {
     color: red;
   }
