826aba7e18657c4d19f89579c18d5e4a0cbf5419
Sultepronit <sultepronit@gmail.com>
2023-10-24 15:16:50 +0300

    added little api text

---
+++ projects/parse-chrom-report/api.js
@@ -0,0 +1,26 @@
+const urlBase = 'https://script.google.com/macros/s/AKfycbxSifZzh0G8Y95_mSfnLsErqS8RUrx73IgJmQvl1Oq81e08p_YeSKDrvoWo_MvljP0sBw/exec';
+
+function postData(sheet, data) {
+	console.log('saving...');
+	const url = urlBase + '?sheet=' + sheet;
+	const params = {
+		method: 'POST',
+		headers: { Accept: 'application/json' },
+		body: JSON.stringify(data)
+	};
+	try {
+		//const resp = await fetch(url, params);
+        fetch(url, params).then(function(resp){
+            console.log('Success!');
+            console.log(resp);
+        });
+		//const report = await resp.json();
+		/* console.log('saved:');
+		console.log(report); */
+	} catch (error) {
+		alert(error);
+	}
+}
+
+const sheet = 'kg';
+postData(sheet, [['A4', 'Hello there!']]);
