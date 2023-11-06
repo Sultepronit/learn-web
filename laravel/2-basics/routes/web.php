dbe03130eb3b49081be4ae67be85190e460593ea
Sultepronit <sultepronit@gmail.com>
2023-11-06 11:03:14 +0200

    copied progress from previous laravel learn project

--- laravel/2-basics/routes/web.php
+++ laravel/2-basics/routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, soluta cupiditate vero reiciendis, atque animi accusantium exercitationem modi mollitia reprehenderit unde architecto. Unde qui ipsum eos deleniti adipisci, corrupti impedit!'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, soluta cupiditate vero reiciendis, atque animi accusantium exercitationem modi mollitia reprehenderit unde architecto. Unde qui ipsum eos deleniti adipisci, corrupti impedit!'
+            ]
+        ]
+    ]);
 });
