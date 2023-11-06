8f88fa1c3e431185a3581efe9f31b251c7bbecee
Sultepronit <sultepronit@gmail.com>
2023-11-06 15:15:40 +0200

    work a little with models and db in laravel project

--- laravel/2-basics/app/Models/Listing.php
+++ laravel/2-basics/app/Models/Listing.php
@@ -2,32 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, soluta cupiditate vero reiciendis, atque animi accusantium exercitationem modi mollitia reprehenderit unde architecto. Unde qui ipsum eos deleniti adipisci, corrupti impedit!'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, soluta cupiditate vero reiciendis, atque animi accusantium exercitationem modi mollitia reprehenderit unde architecto. Unde qui ipsum eos deleniti adipisci, corrupti impedit!'
-            ]
-        ];
-    }
-
-    public static function find($id)
-    {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
