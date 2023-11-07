a8075e56fc681999815b305dff85576c6153ae91
Sultepronit <sultepronit@gmail.com>
2023-11-07 13:10:40 +0200

    filled mysql db in the  laravel project

--- laravel/2-basics/database/seeders/DatabaseSeeder.php
+++ laravel/2-basics/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,12 +13,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
         # php artisan db:seed
-        \App\Models\User::factory()->create([
+        # php artisan migrate:refresh --seed
+
+        \App\Models\User::factory(5)->create();
+        
+        /* \App\Models\User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]); */
+
+        Listing::create([
+            'title' => 'Laravel developer',
+            'tags' => 'laravel, javascript',
+            'company' => 'Acme Crop',
+            'location' => 'Boston, MA',
+            'email' => 'mihal@dom.com',
+            'website' => 'acme.com',
+            'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde vero autem animi omnis esse aut. Earum repellat, ex et dolor officiis modi facere praesentium dolores architecto. Nobis tenetur nulla deserunt.'
+        ]);
+
+        Listing::create([
+            'title' => 'Laravel developer 2',
+            'tags' => 'laravel, javascript',
+            'company' => 'Acme Crop',
+            'location' => 'Boston, MA',
+            'email' => 'mihal@dom.com',
+            'website' => 'acme.com',
+            'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde vero autem animi omnis esse aut. Earum repellat, ex et dolor officiis modi facere praesentium dolores architecto. Nobis tenetur nulla deserunt.'
         ]);
     }
 }
