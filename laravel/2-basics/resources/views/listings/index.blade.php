90dd2bd7a0d497819bf44c8aff4e2f4da6ab4c29
Sultepronit <sultepronit@gmail.com>
2023-11-16 13:38:38 +0200

    refactored a little the laravel project

---
+++ laravel/2-basics/resources/views/listings/index.blade.php
@@ -0,0 +1,29 @@
+<x-layout>
+    @include('partials._hero')
+    @include('partials._search')
+
+    @php
+        $test = 111;
+        //$listings = [];
+    @endphp
+
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+
+    <!-- @if(count($listings) == 0)
+        <p>No listings found</p>
+    @endif -->
+
+    @unless(count($listings) == 0)
+        @foreach($listings as $listing)
+            <!-- <h2>
+                <a href="./listings/{{$listing['id']}}">{{$listing['title']}}</a>
+            </h2>
+            <p>{{$listing['description']}}</p> -->
+            <x-listing-card :listing="$listing" />
+        @endforeach
+    @else
+        <p>No listings found</p>
+    @endunless
+
+    </div>
+</x-layout>
