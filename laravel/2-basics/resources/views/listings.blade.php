404e728fb89428a9bf6f1f19558418c05969b8a3
Sultepronit <sultepronit@gmail.com>
2023-11-16 12:42:40 +0200

    added first controller in the laravel project

--- laravel/2-basics/resources/views/listings.blade.php
+++ laravel/2-basics/resources/views/listings.blade.php
@@ -9,7 +9,6 @@
 @include('partials._hero')
 @include('partials._search')
 
-<!-- <h1>{{$heading}}</h1> -->
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
 <!-- @if(count($listings) == 0)
--- laravel/2-basics/routes/web.php
+++ laravel/2-basics/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
@@ -15,12 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 # all the listings
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('listings', [
-        'heading' => 'Latest Listings',
         'listings' => Listing::all()
     ]);
-});
+}); */
+Route::get('/', [ListingController::class, 'index']);
 
 # single listing
 /* Route::get('/listings/{id}', function($id) {
@@ -35,8 +36,10 @@ Route::get('/', function () {
     }
 }); */
 
-Route::get('/listings/{listing}', function(Listing $listing) {
+/* Route::get('/listings/{listing}', function(Listing $listing) {
     return view('listing', [
         'listing' => $listing
     ]);
-});
+}); */
+
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
