55c5d6a6c6185f4c9762e5b9ca1380bf8fa3ba05
Sultepronit <sultepronit@gmail.com>
2023-11-18 18:10:26 +0200

    added dummy data to the components in vue project

--- vuejs2/2-project/src/App.vue
+++ vuejs2/2-project/src/App.vue
@@ -1,12 +1,26 @@
 <template>
     <MyHeader />
+    <div class="container">
+        <Balance />
+        <income-expenses />
+        <TransactionList />
+        <AddTransaction />
+    </div>
 </template>
 
 <script>
     import MyHeader from './components/MyHeader.vue';
+    import Balance from './components/Balance.vue';
+    import IncomeExpenses from './components/IncomeExpenses.vue';
+    import TransactionList from './components/TransactionList.vue';
+    import AddTransaction from './components/AddTransaction.vue';
     export default {
         components: {
-            MyHeader
+            MyHeader,
+            Balance,
+            IncomeExpenses,
+            TransactionList,
+            AddTransaction
         }
     }
 </script>
---
+++ vuejs2/2-project/src/components/TransactionList.vue
@@ -0,0 +1,9 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li class="minus">
+            Cash -$400
+            <button class="delete-btn">x</button>
+        </li>
+    </ul>
+</template>
