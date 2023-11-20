344ddc22491bb733c0e491c38ed4bcf335093510
Sultepronit <sultepronit@gmail.com>
2023-11-20 10:42:02 +0200

    added some data to the vue project

--- vuejs2/2-project/src/App.vue
+++ vuejs2/2-project/src/App.vue
@@ -3,18 +3,20 @@
     <div class="container">
         <Balance />
         <income-expenses />
-        <TransactionList />
+        <TransactionList :transactions="transactions" />
         <AddTransaction />
     </div>
 </template>
 
-<script>
+<script setup>
+    import { ref } from 'vue';
+
     import MyHeader from './components/MyHeader.vue';
     import Balance from './components/Balance.vue';
     import IncomeExpenses from './components/IncomeExpenses.vue';
     import TransactionList from './components/TransactionList.vue';
     import AddTransaction from './components/AddTransaction.vue';
-    export default {
+    /* export default {
         components: {
             MyHeader,
             Balance,
@@ -22,5 +24,10 @@
             TransactionList,
             AddTransaction
         }
-    }
+    } */
+
+    const transactions = ref([
+        { id: 1, text: 'Flower', amount: -19.99 },
+        { id: 2, text: 'Salary', amount: 30.5 }
+    ]);
 </script>
--- vuejs2/2-project/src/components/TransactionList.vue
+++ vuejs2/2-project/src/components/TransactionList.vue
@@ -1,9 +1,30 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li class="minus">
-            Cash -$400
+        <li
+            v-for="transaction in transactions"
+            :key="transaction.id"
+            :class="transaction.amount < 0 ? 'minus' : 'plus'"
+        >
+            {{ transaction.text }}
+            ${{ transaction.amount }}
             <button class="delete-btn">x</button>
         </li>
     </ul>
 </template>
+
+<script setup>
+    /* const transactions = [
+        { id: 1, text: 'Flower', amount: -19.99 },
+        { id: 2, text: 'Salary', amount: 30.5 }
+    ]; */
+    import { defineProps } from 'vue';
+
+    // const props = defineProps({
+    defineProps({
+        transactions: {
+            type: Array,
+            required: true
+        }
+    });
+</script>
