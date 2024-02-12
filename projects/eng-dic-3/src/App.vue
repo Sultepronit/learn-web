82149a58ff482a3c8208e7be89f3a904c5ac2e3b
Sultepronit <sultepronit@gmail.com>
2024-02-12 11:25:52 +0200

    added saving history to localStorage

--- projects/eng-dic-3/src/App.vue
+++ projects/eng-dic-3/src/App.vue
@@ -2,56 +2,99 @@
 import { ref } from 'vue';
 import getArticle from '@/services/getArticle.js'
 
-const query = ref('apple');
+const query = ref('');
 const e2uArticle = ref('');
 const glosbeArticle = ref('');
 const selectedArticle = ref('e2u');
+let history = [];
+
+const theInput = ref(null);
+
+function getHistory() {
+    const json = localStorage.getItem('EngDicHistory');
+    if(json) {
+        history = JSON.parse(json);
+        query.value = history[history.length - 1];
+        request(query.value);
+    }
+}
+getHistory();
+
+function updateHistory() {
+    if(history[history.length - 1] !== query.value) {
+        history.push(query.value);
+        console.log(history);
+        localStorage.setItem('EngDicHistory', JSON.stringify(history));
+    }
+}
 
 async function request(input) {
     query.value = input;
-    console.log(query);
-    
+    // console.log(query);
+
+    e2uArticle.value = '';
+    glosbeArticle.value = '---';
     selectedArticle.value = 'e2u';
-    // const art = await getArticle('e2u', query.value);
-    // const art = await getArticle('glosbe', query.value);
-    // console.log(art);
-    await getArticle('e2u', query.value).then(article => {
-        e2uArticle.value = article;
-    });
-    await getArticle('glosbe', query.value).then(article => {
-        glosbeArticle.value = article;
-    });
+
+    e2uArticle.value = await getArticle('e2u', query.value);
+
+    theInput.value.select();
+
+    updateHistory();
 }
 
-function selected(dic) {
+async function getGlosbe() {
+    glosbeArticle.value = await getArticle('glosbe', query.value);
+}
+
+function select(dic) {
     console.log(dic);
     selectedArticle.value = dic;
+
+    if(dic === 'glosbe' && glosbeArticle.value === '---') {
+        getGlosbe();
+    }
 }
-request(query.value);
+
 </script>
 
 <template>
     <header>
         <input
             type="text"
+            ref="theInput"
             class="the-input"
             :value="query"
             @change="request($event.target.value)"
         >
+        <div class="tabs">
+            <p
+                class="tab"
+                :class="selectedArticle === 'e2u' ? 'selectedTab' : ''"
+                @click="select('e2u')"
+            >
+                e2u
+            </p>
+            <p
+                class="tab"
+                :class="selectedArticle === 'glosbe' ? 'selectedTab' : ''"
+                @click="select('glosbe')"
+            >
+                glosbe
+            </p>
+        </div>
     </header>
 
     <main>
         <div
             class="e2u"
-            :class="selectedArticle === 'e2u' ? 'selected' : 'unselected'"
+            v-show="selectedArticle === 'e2u'"
             v-html="e2uArticle"
-            @click="selected('e2u')"
         />
         <div
             class="glosbe"
-            :class="selectedArticle === 'glosbe' ? 'selected' : 'unselected'"
+            v-show="selectedArticle === 'glosbe'"
             v-html="glosbeArticle"
-            @click="selected('glosbe')"
         />
     </main>
 </template>
@@ -59,36 +102,59 @@ request(query.value);
 <style>
 .the-input {
     font-size: 2rem;
+    max-width: 100%;
 }
-.selected {
-    max-height: 70vh;
+.tabs {
+   display: flex; 
 }
-.unselected {
-    max-height: calc(30vh - 4rem - 5px);
+.tab {
+    width: 4em;
+    border: 3px solid gray;
+    /* background: #dadada; */
+    margin: 0.2rem;
+    text-align: center;
 }
-.e2u {
-    /* max-height: 80vh; */
+.selectedTab {
+    border-color: blue;
+}
+
+/* .e2u {
     overflow: auto;
     margin-bottom: 1rem;
-}
+} */
 
 table {
     width: 100%;
 }
 
 table.other {
-    border-left: 3px solid red;
+    border-left: 4px solid orange;
+}
+
+table.context {
+    border-left: 4px solid red;
 }
 
 td {
     border-bottom: 2px solid;
 }
 
-.glosbe {
+.result_row .found {
+    color: blue;
+}
+
+/* .glosbe {
     overflow: auto;
+} */
+
+.glosbe-eng {
+    background: #fdfdab;
 }
 
 .glosbe-source {
     text-align: right;
+    font-size: 0.7em;
+    border-bottom: 2px solid;
+    margin-bottom: 0.2rem;
 }
 </style>
