ca17de0b49d2baed3c1bcf3f527c76d854728ddb
Sultepronit <sultepronit@gmail.com>
2024-02-19 15:43:21 +0200

    created simplest page that tests http requests and shows results in the console

---
+++ train/node-php-layer/test-request/main.js
@@ -0,0 +1,43 @@
+// const url = 'http://localhost:8080/';
+const url = 'http://localhost:5555/';
+async function get() {
+    const json = await fetch(url);
+    console.log(json);
+    const resp = await json.json();
+    console.log(resp);
+}
+get();
+
+async function post() {
+    const data = {a: 1, b: 2};
+    const json = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data)
+    });
+    console.log(json);
+    const resp = await json.json();
+    console.log(resp);
+}
+
+async function patch() {
+    const data = {a: 1};
+    const json = await fetch(url + '?id=1', {
+        method: 'PATCH',
+        body: JSON.stringify(data)
+    });
+    console.log(json);
+    const resp = await json.json();
+    console.log(resp);
+}
+
+async function del() {
+    const json = await fetch(url + '?id=1', {
+        method: 'DELETE',
+    });
+    console.log(json);
+    const resp = await json.json();
+    console.log(resp);
+}
+post();
+patch();
+del();
