53b527a61dd24e8198f92ea7886540db04d41868
Sultepronit <sultepronit@gmail.com>
2024-02-24 12:47:57 +0200

    figured out hot to connect to db container; experimented a little

--- docker/1-basics/app/public/p.php
+++ docker/1-basics/app/public/p.php
@@ -1,12 +1,20 @@
 <?php
 
-$host = '127.0.0.1'; // or 'localhost' if MySQL server is running on the same machine
+echo 'Here we go!';
+// $host = '127.0.0.1'; // or 'localhost' if MySQL server is running on the same machine
+// $host = 'localhost';
+// $host = '0.0.0.0';
+$host = 'db';
 $dbname = 'docker-php';
 $username = 'root';
 $password = 'secret';
-$port = 3316; // Specify the port number
+$port = 3306; // Specify the port number
 
 $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
 
+print_r($pdo);
+
 echo '<pre>';
 print_r($_SERVER);
+
+echo file_get_contents('https://example.com');
