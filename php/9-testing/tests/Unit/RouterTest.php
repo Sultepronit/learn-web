50d45dff2fec492d0f62e230f332ebe34ef5fca9
Sultepronit <sultepronit@gmail.com>
2024-03-24 18:31:33 +0000

    fixed bug found with test; refactored tests using setUp()

--- php/9-testing/tests/Unit/RouterTest.php
+++ php/9-testing/tests/Unit/RouterTest.php
@@ -9,16 +9,22 @@ use App\Router;
 
 class RouterTest extends TestCase
 {
+    private Router $router;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->router = new Router();
+    }
+
     # two ways of delcaring test: /** @test */ or test_that_...
     // /** @test */
     // public function it_registers_a_route(): void
     public function test_that_it_registers_a_route(): void
     {
-        // given that we have a router object
-        $router = new Router();
-
         // when we call a register method
-        $router->register('get', '/users', ['Users', 'index']);
+        $this->router->register('get', '/users', ['Users', 'index']);
 
         $expected = [
             'get' => [
@@ -27,15 +33,13 @@ class RouterTest extends TestCase
         ];
 
         // then we assert route was registered
-        $this->assertEquals($expected, $router->routes());
+        $this->assertEquals($expected, $this->router->routes());
     }
 
     /** @test */
     public function itRegistersAGetRoute(): void
     {
-        $router = new Router();
-
-        $router->get('/users', ['Users', 'index']);
+        $this->router->get('/users', ['Users', 'index']);
 
         $expected = [
             'get' => [
@@ -43,15 +47,13 @@ class RouterTest extends TestCase
             ]
         ];
 
-        $this->assertEquals($expected, $router->routes());
+        $this->assertEquals($expected, $this->router->routes());
     }
 
     /** @test */
     public function itRegistersAPostRoute(): void
     {
-        $router = new Router();
-
-        $router->post('/users', ['Users', 'store']);
+        $this->router->post('/users', ['Users', 'store']);
 
         $expected = [
             'post' => [
@@ -59,6 +61,12 @@ class RouterTest extends TestCase
             ]
         ];
 
-        $this->assertEquals($expected, $router->routes());
+        $this->assertEquals($expected, $this->router->routes());
+    }
+
+    /** @test */
+    public function there_are_no_routes_when_router_is_created(): void
+    {
+        $this->assertEmpty((new Router())->routes());
     }
 }
