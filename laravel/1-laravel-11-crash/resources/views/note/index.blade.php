9f92372429f46b32e76378b8cb27dd6b6c0904e6
Sultepronit <sultepronit@gmail.com>
2024-03-27 10:09:33 +0000

    now the index looks and works fine enough

--- laravel/1-laravel-11-crash/resources/views/note/index.blade.php
+++ laravel/1-laravel-11-crash/resources/views/note/index.blade.php
@@ -4,12 +4,14 @@
         <div class="notes">
             @foreach ($notes as $note)
                 <div class="note">
-                    {{ Str::words($note->note, 30) }}
-                </div>
-                <div class="note-buttons">
-                    <a href="{{ route('note.show', $note) }}" class="note-edit-button">View</a>
-                    <a href="{{ route('note.edit', $note) }}" class="note-edit-button">Edit</a>
-                    <button class="note-delete-button">Delete</button>
+                    <div class="note-body">
+                        {{ Str::words($note->note, 30) }}
+                    </div>
+                    <div class="note-buttons">
+                        <a href="{{ route('note.show', $note) }}" class="note-edit-button">View</a>
+                        <a href="{{ route('note.edit', $note) }}" class="note-edit-button">Edit</a>
+                        <button class="note-delete-button">Delete</button>
+                    </div>
                 </div>
             @endforeach
         </div>
