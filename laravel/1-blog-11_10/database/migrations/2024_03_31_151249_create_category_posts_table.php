61363e844fd8748f230238253018a1b7777be7f6
Sultepronit <sultepronit@gmail.com>
2024-04-08 12:26:24 +0000

    laravel blog: adjusted forms and tables

--- laravel/1-blog-11_10/database/migrations/2024_03_31_151249_create_category_posts_table.php
+++ laravel/1-blog-11_10/database/migrations/2024_03_31_151249_create_category_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('category_post', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreignId('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('category_posts');
+        Schema::dropIfExists('category_post');
     }
 };
