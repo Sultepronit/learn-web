2e44b8d45d72e67eafec3903b3d7d7e96a54d5b9
Sultepronit <sultepronit@gmail.com>
2024-04-14 18:20:48 +0000

    laravel blog: created text widgets

--- laravel/1-blog-11_10/resources/views/home.blade.php
+++ laravel/1-blog-11_10/resources/views/home.blade.php
@@ -3,15 +3,18 @@
 ?>
 
 <x-app-layout>
-<!-- Posts Section -->
-<section class="w-full md:w-2/3 flex flex-col items-center px-3">
+    <!-- Posts Section -->
+    <section class="w-full md:w-2/3 flex flex-col items-center px-3">
 
-    @foreach ($posts as $post)
-        <x-post-item :post="$post"></x-post-item>
-    @endforeach
+        @foreach ($posts as $post)
+            <x-post-item :post="$post"></x-post-item>
+        @endforeach
 
-    <!-- Pagination -->
-    {{$posts->onEachSide(1)->links()}}
-    
-</section>
+        <!-- Pagination -->
+        {{$posts->onEachSide(1)->links()}}
+        
+    </section>
+
+    <x-sidebar />
+
 </x-app-layout>
