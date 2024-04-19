9ee42cefc94ba551a078270dfb04146f845a9921
Sultepronit <sultepronit@gmail.com>
2024-04-19 16:41:07 +0000

    laravel blog: created the page for the post

--- laravel/1-blog-11_10/routes/web.php
+++ laravel/1-blog-11_10/routes/web.php
@@ -8,3 +8,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('/{post:slug}', [PostController::class, 'show'])->name('view');
