aa255f8a37133100f7edeb177a839dfbcd3751de
Sultepronit <sultepronit@gmail.com>
2024-08-03 20:22:36 +0300

    little changes

--- train/vue-chartjs/src/components/BarChart0.vue
+++ train/vue-chartjs/src/components/BarChart0.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { ref, computed } from 'vue';
 import { Bar } from 'vue-chartjs';
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+// import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+// ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+import { Chart, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+Chart.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
 const chartOptions = {
     responsive: true,
